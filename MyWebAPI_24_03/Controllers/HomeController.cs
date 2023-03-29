e2aa3d315516be7c2080713afd5c90ef1e8f51cf
TPONG <tuanphong01112002@gmail.com>
2023-03-29 09:37:07 +0700

    Add project files.

---
+++ MyWebAPI_24_03/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyWebAPI_24_03.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
